47fa777d6de16b18f5f5ed81b1ea4d854eaac2f0
Evelyn Scidmore <escidmore@gmail.com>
2021-01-16 12:51:52 -0800

    Fixing some broken files

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,4 @@
 # Path to Oh My Fish install.
-<<<<<<< HEAD
 set -gx OMF_PATH "/Users/scidmev/.local/share/omf"
 
 # Customize Oh My Fish configuration path.
@@ -24,16 +23,3 @@ set FILTER percol
 
 # Init jenv
 set PATH $HOME/.jenv/bin $HOME/.jenv/shims $PATH
-=======
-set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "$HOME/.config/omf"
-
-set fish_greeting ""
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
-[ -f /usr/share/autojump/autojump.fish ]; and source /usr/share/autojump/autojump.fish
->>>>>>> b11dfbb17755f04a4c1b9c00d92b742bb744a8d6
