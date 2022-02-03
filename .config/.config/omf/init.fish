0768b893e3e3ae3af6c75545c6185222a1e0c22f
Evelyn Scidmore <escidmore@gmail.com>
2022-02-03 13:29:08 -0800

    Oops

--- .config/.config/omf/init.fish
+++
@@ -1,5 +0,0 @@
-set -g tab_iterm_profile "Default"
-
-# Expand plugin configurations.  Expand-word definitions, then functions
-expand-word -p '^s/..*/.*$' -e 'echo -n "$history[1]" | sed -e (commandline -t)/g'
-# End Expand plugin configurations
